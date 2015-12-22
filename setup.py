b38c4e2ba326f173b58bd9ee5eec5ffa7a9c96e4
Exel232 <alcasa.mz@gmail.com>
2015-12-22 23:58:46 +0100

    Tweak compilation for Windows /

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ setup(
     windows=[{
         "script": 'UI.py',
         "icon_resources": [{1, "link_icon.ico"}]
-    }]
+    }],
+    options={
+        "py2exe":{
+            "bundle_files": 3
+        }
+        }
     )
